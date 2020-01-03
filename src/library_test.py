34d37cfbe7c127f718c708dcec01b6c01135b232
Kerem Yilmaz <keremyilmazcs@gmail.com>
2020-01-04 00:58:46 +0300

    Using custom transformers

--- src/library_test.py
+++ src/library_test.py
@@ -2,20 +2,22 @@ import torch
 from torchvision import transforms, datasets
 
 from src.helpers import get_label
+from src.preprocessing import Resize, Padding, Normalize
 
 data_transform = transforms.Compose(
     [
-        transforms.Resize([224, 224]),
+        Resize(desired_dimension=224),
         transforms.ToTensor(),
-        # transforms.Normalize(mean=[0.485, 0.456, 0.406],
-        #                      std=[0.229, 0.224, 0.225])
+        Padding(output_size=[224, 224]),
+        Normalize(mean=[0.485, 0.456, 0.406],
+                  std=[0.229, 0.224, 0.225]),
     ]
 )
 
 image_net_dataset = datasets.ImageFolder(root="../data/train", transform=data_transform)
 
 dataset_loader = torch.utils.data.DataLoader(
-    image_net_dataset, batch_size=1, shuffle=True
+    image_net_dataset, batch_size=1, shuffle=False
 )
 
 dataset_iter = iter(dataset_loader)
@@ -27,9 +29,6 @@ to_pil = transforms.ToPILImage()
 
 im = batch[0][0]
 im = to_pil(im)
-# im = np.resize(im, (224, 224, 3))
-# im = im.view(224, 224, -1)
-# print(im.size())
 plt.figure()
 plt.imshow(im)
 plt.show()
